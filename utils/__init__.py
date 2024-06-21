6309d1d06a5421148dddbe9b7519f6242ea2a197
ifnesi <inesi@confluent.io>
2024-06-21 17:46:17 +0100

    Minor

--- utils/__init__.py
+++ utils/__init__.py
@@ -210,5 +210,13 @@ def sys_exc(exc_info):
 
 
 def datetime_handler(o):
-    if isinstance(o, (datetime.datetime, datetime.date, datetime.time, bytes)):
+    if isinstance(
+        o,
+        (
+            datetime.datetime,
+            datetime.date,
+            datetime.time,
+            bytes,
+        ),
+    ):
         return str(o)
