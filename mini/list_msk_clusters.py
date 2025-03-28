0ec23b7e29ca3707c79174b85e785a2c097d0229
Justin Lee <justin.r.lee@gmail.com>
2025-03-28 12:04:43 +0800

    Create list_msk_clusters.py

---
+++ mini/list_msk_clusters.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2020 Confluent Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# BY DEFAULT, WILL ONLY RUN AGAINST TEST INSTANCES
+# MUST USE --full FLAG TO RUN AGAINST WHOLE ACCOUNT
+
+# On fresh Ubuntu instance, install prereqs:
+# sudo apt-get update; sudo apt-get install -y python3-pip;
+# python3 -m pip install -r requirements.txt
+#
+import os
+import sys
+import json
+import yaml
+import logging
+import argparse
+import datetime
+import boto3
+import time
+
+if __name__ == "__main__":
+    region_client = boto3.client(
+        service_name = "ec2"
+    )
+
+    regions = [region.get("RegionName") for region in region_client.describe_regions().get("Regions")]
+
+    for region in regions:
+        kafka_client = boto3.client(
+            service_name = "kafka",
+            region_name = region
+        )
+
+        clusters = kafka_client.list_clusters_v2(
+            MaxResults = 100,
+        ).get("ClusterInfoList")
+
+        for cluster in clusters:
+            print("{};{};{};{}".format(
+                region,
+                cluster.get('ClusterType'),
+                cluster.get('ClusterName'),
+                cluster.get('CreationTime'),
+                cluster.get('tags')
+            ))
